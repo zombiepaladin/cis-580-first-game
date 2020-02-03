09465498c733f7271a3a4d64086699c5e5b4b579
amandajoelleyoung <nhbean@ksu.edu>
2020-02-03 11:33:34 -0600

    In-class work from 1/31/2020

---
+++ MonoGameWindowsStarter/BoundingCircle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameWindowsStarter
+{
+    public struct BoundingCircle
+    {
+        public float X;
+
+        public float Y;
+
+        public float Radius;
+
+        public Vector2 Center {
+            get => new Vector2(X, Y);
+            set { 
+                X = value.X; 
+                Y = value.Y; 
+            }
+        }
+
+    }
+}
---
+++ MonoGameWindowsStarter/BoundingRectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameWindowsStarter
+{
+    public struct BoundingRectangle
+    {
+        public float X;
+
+        public float Y;
+
+        public float Width;
+
+        public float Height;
+
+        public BoundingRectangle(float x, float y, float width, float height)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public bool CollidesWith(BoundingRectangle other)
+        {
+            return !(this.X > other.X + other.Width
+                  || this.X + this.Width < other.X
+                  || this.Y > other.Y + other.Height
+                  || this.Y + this.Height < other.Y);
+        }
+
+    }
+
+    
+}
